12681e48a731db638e37f0a1b7c4a09904e9c661
Sanjeev <umarsanjeev@gmail.com>
2022-02-23 19:57:07 +0530

    fix

--- script.js
+++ script.js
@@ -1,49 +1,69 @@
+import { API_KEY } from "./Api_key";
+
 const info = document.querySelectorAll(".details");
 const container = document.querySelector(".container");
-
+const loadingRipple = ` 
+<div class="lds-ripple">
+<div></div>
+<div></div>
+</div>`;
 const getWeather = async (country) => {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=1075b7c5f2dc436ca7a92921220902&q=${country}&aqi=no`
-  );
+  try {
+    container.innerHTML = "";
+    container.insertAdjacentHTML("afterbegin", loadingRipple);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${country}&units=metric&appid=${config.API_KEY}`
+    );
+    if (!response.ok) {
+      throw new Error("Data not found");
+    }
 
-  const data = await response.json();
-
-  return data;
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    throw new Error(e.message);
+  }
 };
 
 const render = (data) => {
-  console.log(data);
-
   const card = `
     <div class="card" >
       <div class ="top"> 
-        <h2 class="country">Weather in ${data.location.name}</h2>
+        <h2 class="country">Weather in ${data.name}</h2>
       </div>
 
       <div class="icon">
-      <img src="${data.current.condition.icon}" alt="icon" />
+      <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png
+      " alt="icon" />
       </div>
 
       <div class="details tempinfo">
-        <span>${data.current.temp_c}°C</span>
+        <span>${data.main.temp}°C</span>
       </div>
 
       
 
       <div class="container-info" >
         <div class="details flinfo">
+        <p>Min temp</p>
+        <span>${data.main.temp_min}°C</span>
+        </div>
+        
+        <div class="details huminfo">
+          <p>Max temp</p>
+          <span>${data.main.temp_max}°C</span>
+        </div>
+      <div class="container-info" >
+        <div class="details flinfo">
         <p>FeelsLike</p>
-        <span>${data.current.feelslike_c}</span>
+        <span>${data.main.feels_like}</span>
         </div>
         
         <div class="details huminfo">
           <p>Humidity</p>
-          <span>${data.current.humidity}</span>
+          <span>${data.main.humidity}%</span>
         </div>
-        <div class="details windinfo">
-          <p>Wind</p>
-          <span>${data.current.wind_degree}°</span>
-        </div>
+       
       </div>
     </div>`;
 
@@ -60,7 +80,13 @@ button.addEventListener("click", (e) => {
   e.preventDefault();
   const query = text.value;
   if (query) {
-    getWeather(query).then((data) => render(data));
+    getWeather(query)
+      .then((data) => render(data))
+      .catch((e) => {
+        const error = `<h2>${e.message}</h2>`;
+        container.innerHTML = "";
+        container.insertAdjacentHTML("beforeend", error);
+      });
 
     document.querySelector(
       "body"
